18ea391e6e7f808e9d198d9cb34e50cfbfe448e0
Vincent Carrier <vincentcarrier15@gmail.com>
2025-01-14 10:25:02 -0500

    Typography fixes

--- src/routes/equiterre/+page.svelte
+++ src/routes/equiterre/+page.svelte
@@ -93,17 +93,17 @@
 			<section class="flex pb-16">
 				<div class="mt-3 h-5 w-5 shrink-0 rounded-full bg-orange-500"></div>
 				<p class="ml-6 mr-4 max-w-sm text-xl font-semibold">
-					<strong class="block text-4xl"> D'ici 3 ans, </strong>
+					<strong class="block text-4xl"> D’ici 3 ans, </strong>
 					les gens pourraient être incapables de distinguer ce qui est vrai de ce qui ne l'est pas, selon
 					Horizons de politiques Canada.
 				</p>
 				<div class="flex-grow"></div>
 				<div class="max-w-lg text-2xl">
 					<p>
-						<strong>Bien s'informer est plus important que jamais.</strong>
+						<strong>Bien s’informer est plus important que jamais.</strong>
 					</p>
 					<p class="mt-6 text-gray-700">
-						Que ce soit pour se protéger des belles promesses d'un faux spa ou pour éviter d'être
+						Que ce soit pour se protéger des belles promesses d’un faux spa ou pour éviter d'être
 						manipulé à des fins politiques, apprendre à discerner le vrai du faux est devenu
 						primordial.
 					</p>
@@ -118,7 +118,7 @@
 			<h2
 				class="mb-8 text-balance text-center text-[2.5rem] font-medium leading-tight text-lime-200"
 			>
-				En vérifiant l'information,<br /> Équiterre vous aide à distinguer le vrai du faux.
+				En vérifiant l’information,<br /> Équiterre vous aide à distinguer le vrai du faux.
 			</h2>
 			<form action="">
 				<div class="flex justify-center gap-x-4">
